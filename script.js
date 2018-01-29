fb31a0d0f8f557eac501bfa7928c5c18df67d38e
Hannah Jeror <jerorhn@yahoo.com>
2018-01-29 14:58:18 -0500

    Randomize pickedColor

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var colors = [
   'rgb(255, 0, 255)',
 ];
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 
@@ -34,3 +34,8 @@ function changeColors(color) {
     squares[i].style.backgroundColor = color;
   }
 };
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
