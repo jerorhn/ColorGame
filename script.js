9d59e0955161f00eb5b6a60147a78892ab85f252
Hannah Jeror <jerorhn@yahoo.com>
2018-01-29 14:49:12 -0500

    Add design to selections

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var colors = [
 var squares = document.querySelectorAll('.square');
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,9 +20,17 @@ for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener ('click', function() {
     var clickedColor = this.style.backgroundColor;
     if (clickedColor == pickedColor) {
-      alert('Correct!');
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
     } else {
-      alert('Wrong!');
+      this.style.backgroundColor = '#232323';
+      messageDisplay.textContent = "Try Again";
     }
-  })
+  });
 }
+
+function changeColors(color) {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+};
